a56ca09185b2617cd6a6ddf80d576f2e3f960e98
som-syom <dhyeon@student.42seoul.kr>
2022-01-16 21:09:12 +0900

    Feat - media 768 적용

--- src/NaviBar.js
+++ src/NaviBar.js
@@ -3,52 +3,81 @@ import "./stylesheets/NaviBar.css";
 import IconMenu from "./assets/icon-menu.png";
 import TitleLogo from "./assets/wanted_Logo_black.png";
 import SearchBtn from "./assets/search.svg";
+import NewIcon from "./assets/new.svg";
+import BetaIcon from "./assets/beta.svg";
+import MenuBtn from "./assets/dot.svg";
 
 function NaviBar() {
   return (
     <>
-      <section className="navibar-wrap">
-        <div className="navibar-container">
-          <div className="navibar__left">
-            <button className="navibar__left__menu-btn">
-              <img
-                src={IconMenu}
-                alt="hamberger menu"
-                className="navibar__left__menu-btn__hamberger"
-              />
-            </button>
-            <button className="navibar__left__menu-btn">
-              <img
-                src={TitleLogo}
-                alt="wanted title logo"
-                className="navibar__left__menu-btn__title"
-              />
-            </button>
+      <header className="navibar">
+        <section className="navibar-wrap">
+          <div className="navibar-container">
+            <div className="navibar__left">
+              <div className="navibar__left__default">
+                <div className="navibar__left__hamberger-wrap">
+                  <button className="navibar__left__menu-btn">
+                    <img
+                      src={IconMenu}
+                      alt="hamberger menu"
+                      className="navibar__left__menu-btn__hamberger"
+                    />
+                  </button>
+                </div>
+                <button className="navibar__left__menu-btn">
+                  <img
+                    src={TitleLogo}
+                    alt="wanted title logo"
+                    className="navibar__left__menu-btn__title"
+                  />
+                </button>
+              </div>
+              <button className="navibar__left__signup-btn">
+                회원가입하기
+              </button>
+            </div>
+            <ul className="navibar__middle">
+              <li className="navibar__middle__menu-btn btn">채용</li>
+              <li className="navibar__middle__menu-btn btn">이벤트</li>
+              <li className="navibar__middle__menu-btn btn">직군별 연봉</li>
+              <li className="navibar__middle__menu-btn btn">이력서</li>
+              <li className="navibar__middle__menu-btn btn">
+                커뮤니티
+                <em>
+                  <img src={NewIcon} alt="new" />
+                </em>
+              </li>
+              <li className="navibar__middle__menu-btn btn">프리랜서</li>
+              <li className="navibar__middle__menu-btn btn">
+                AI 합격예측
+                <em>
+                  <img src={BetaIcon} alt="beta" />
+                </em>
+              </li>
+            </ul>
+            <div className="navibar__right">
+              <button className="navibar__right__menu-btn">
+                <img
+                  src={SearchBtn}
+                  alt="search button"
+                  className="navibar__right__menu-btn__search"
+                />
+              </button>
+              <button className="navibar__right__menu-btn navibar__signin-btn">
+                회원가입/로그인
+              </button>
+              <div className="navibar__right__menu-btn__division">
+                <button className="navibar__right__menu-btn navibar__dashboard-btn">
+                  기업서비스
+                </button>
+              </div>
+              <button className="navibar__right__menu-btn__menu">
+                <img src={MenuBtn} alt="menu" />
+              </button>
+            </div>
           </div>
-          <div className="navibar__middle">
-            <button className="navibar__middle__menu-btn">채용</button>
-            <button className="navibar__middle__menu-btn">이벤트</button>
-            <button className="navibar__middle__menu-btn">직군별 연봉</button>
-            <button className="navibar__middle__menu-btn">이력서</button>
-            <button className="navibar__middle__menu-btn">커뮤니티</button>
-            <button className="navibar__middle__menu-btn">프리랜서</button>
-            <button className="navibar__middle__menu-btn">AI 합격예측</button>
-          </div>
-          <div className="navibar__right">
-            <button className="navibar__right__menu-btn">
-              <img
-                src={SearchBtn}
-                alt="search button"
-                className="navibar__right__menu-btn__search"
-              />
-            </button>
-            <button className="navibar__right__menu-btn">
-              회원가입/로그인
-            </button>
-            <button className="navibar__right__menu-btn">기업서비스</button>
-          </div>
-        </div>
-      </section>
+        </section>
+      </header>
     </>
   );
 }
