cfddd57b5b2d4f3388ad71059c5089db9e7a2530
som-syom <dhyeon@student.42seoul.kr>
2022-01-18 01:53:41 +0900

    FEAT - prev next btn 추가

--- src/Slide.js
+++ src/Slide.js
@@ -1,18 +1,36 @@
 import React from "react";
-// import { PropTypes } from "prop-types";
+import { PropTypes } from "prop-types";
+import { text } from "./images";
+import GoIcon from "./assets/goto.svg";
 
-function Slide(BannerImage) {
+function Slide({ image, idx }) {
   return (
     <>
-      <div className="banner-image">
-        <img src={BannerImage} alt="banner" />
+      <div className="banner-wrap" key={idx}>
+        <div className="banner-image">
+          <img src={image} alt="banner" />
+        </div>
+        <div className="banner-text">
+          <h2 className="banner-text__h2">{text[idx].h2}</h2>
+          <h3 className="banner-text__h3">{text[idx].h3}</h3>
+          <hr className="banner-text__hr"></hr>
+          <a href="/" className="banner-text__go">
+            <span className="banner-text__go__text">바로가기</span>
+            <img
+              src={GoIcon}
+              alt="바로가기"
+              className="banner-text__go__icon"
+            />
+          </a>
+        </div>
       </div>
     </>
   );
 }
 
-// Slide.propTypes = {
-//   BannerImage: PropTypes.node.isRequired,
-// };
+Slide.propTypes = {
+  image: PropTypes.node.isRequired,
+  idx: PropTypes.number.isRequired,
+};
 
 export default Slide;
--- src/SlideBanner.js
+++ src/SlideBanner.js
@@ -1,10 +1,12 @@
-import React from "react";
-// import Slide from "./Slide";
+import React, { useRef } from "react";
+import Slide from "./Slide";
 import "./stylesheets/SlideBanner.css";
 import { img } from "./images";
+import nextBtn from "./assets/right.svg";
+import prevBtn from "./assets/left.svg";
 
 function SlideBanner() {
-  // const sliderRef = useRef();
+  const sliderRef = useRef();
   // const [currentBanner, setCurrentBanner] = useState([8, 0, 1]);
   let images = Object.keys(img).map((key) => img[key]);
 
@@ -18,18 +20,18 @@ function SlideBanner() {
   return (
     <>
       <section className="slider-container">
-        <div className="slider-images">
+        <div className="slider-images" ref={sliderRef}>
           {images.map((image, idx) => {
-            return (
-              <div className="banner-image" key={idx}>
-                <img src={image} alt="banner" />
-              </div>
-            );
+            return <Slide image={image} idx={idx} key={idx} />;
           })}
         </div>
 
-        <button className="prev-btn" onClick={onClickPrevBtn}></button>
-        <button className="next-btn" onClick={onClickNextBtn}></button>
+        <button className="prev-btn" onClick={onClickPrevBtn}>
+          <img src={prevBtn} alt="prev" />
+        </button>
+        <button className="next-btn" onClick={onClickNextBtn}>
+          <img src={nextBtn} alt="next" />
+        </button>
       </section>
     </>
   );
